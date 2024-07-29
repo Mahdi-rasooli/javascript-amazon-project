cd6708b38e774bf6b5e151d7d798a38e3dbcffd9
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-07-29 16:26:48 +0330

    Create automated test for format currency

---
+++ test/moneyTest.js
@@ -0,0 +1,27 @@
+import { formatCurrency } from '../javascripts/utils/money.js'
+
+
+console.log('test suit : format currency');
+
+console.log('convert cents into dollar');
+if(formatCurrency(2095) === '20.95'){
+    console.log('passed');
+}  else{
+    console.log('failed');
+};
+
+
+console.log('work with zero');
+if(formatCurrency(0) === '0.00'){
+    console.log('passed');
+}  else{
+    console.log('failed');
+};
+
+
+console.log('round up to nearest cents');
+if(formatCurrency(2000.5) === '20.01'){
+    console.log('passed');
+}  else{
+    console.log('failed');
+};
