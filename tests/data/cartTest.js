3ca5be5b23dfd490d9178e9edf9bbbf038270f57
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-08-05 19:58:00 +0330

    Load products in beforeAll hooks

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -96,7 +96,7 @@ describe('test suite : remove from cart', () => {
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
         expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
           productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-          quantity: 1,
+          quantity: 2,
           deliveryOptionId: '1'
         }]));
     })
